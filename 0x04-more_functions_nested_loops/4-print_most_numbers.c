af51e341f653e7f0715fd4158d6c65c97266bfc5
ziphozenkos <ziphncayiyana@gmail.com>
2023-02-23 14:39:49 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num exclude 2 nd 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (!(i == '2' || i == '4'))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
