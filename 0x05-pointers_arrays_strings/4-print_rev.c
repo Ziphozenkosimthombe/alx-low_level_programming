4bda817faf48a14c26d169cbe0e81d6cf5e434bc
ziphozenkos <ziphncayiyana@gmail.com>
2023-02-27 11:12:06 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print a string in reverse
+ * @s: the string to pring
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+	putchar(s[c]);
+	}
+	putchar('\n');
+}
