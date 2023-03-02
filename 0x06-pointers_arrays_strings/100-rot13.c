a954d99d1a1532ca692d27ec38f8546cf389e70c
ziphozenkos <ziphncayiyana@gmail.com>
2023-03-02 14:46:26 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @str: input
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (str[i] == a[j])
+	{
+	str[i] = b[j];
+	break;
+	}
+	}
+	}
+	return (str);
+}
+
+
+
