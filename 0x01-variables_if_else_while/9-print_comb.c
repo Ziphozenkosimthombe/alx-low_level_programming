3f6bf14a32717b0e70a7c4d3eed397a176632cad
ziphozenkos <ziphncayiyana@gmail.com>
2023-03-04 07:37:30 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		if (i != 10)
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
