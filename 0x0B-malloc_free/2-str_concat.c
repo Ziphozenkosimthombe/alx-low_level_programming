9f78ceccb702cb0394ac4ac978fc27de76adff45
ziphozenkos <ziphncayiyana@gmail.com>
2023-03-14 08:50:20 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * str_concat - cancatenate two strings
- * @s1: input
- * @s2: input
- * Return: a pointer shold point newly allocated
+ * str_concat - function declaration
+ * @s1: string 1
+ * @s2: string 2
+ * Return: as string value
+ */
+
+char *str_concat(char *s1, char *s2);
+
+/**
+ * str_concat - function definition
+ * @s1: the first string
+ * @s2: the second string
+ * Description: concatenates s1 and s2
+ * Return: the concatenated string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	int i = 0;
-	int j;
+	char *new_string;
+	int index;
+	int concat_index = 0;
+	int len = 0;
 
-	if (!s1)
-		s1 = " ";
-	if (!s2)
-		s2 = " ";
+	if (s1 == NULL)
+		s1 = "";
 
-	result = malloc(strlen(s1) + strlen(s2) + 1);
-	if (!result)
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	new_string  = malloc(sizeof(char) * len);
+
+	if (new_string == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		result[i] = s1[i];
-	}
-	for (j = 0; s2[j] != '\0'; j++, i++)
-	{
-		result[i] = s2[j];
-	}
-	result[i] = '\0';
-	return (result);
+	for (index = 0; s1[index]; index++)
+		new_string[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		new_string[concat_index++] = s2[index];
+
+	return (new_string);
 }
