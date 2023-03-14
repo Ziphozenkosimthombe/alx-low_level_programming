13f4fe595c4bf499591f47a13eb75fd120bd2b04
ziphozenkos <ziphncayiyana@gmail.com>
2023-03-14 07:51:09 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,19 +14,13 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 
 	if (!s1)
-	{
 		s1 = " ";
-	}
 	if (!s2)
-	{
 		s2 = " ";
-	}
 
 	result = malloc(strlen(s1) + strlen(s2) + 1);
 	if (!result)
-	{
 		return (NULL);
-	}
 	strcpy(result, s1);
 	strcat(result, s2);
 	return (result);
