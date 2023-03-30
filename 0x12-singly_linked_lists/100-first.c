39a29336c958c16a72c6160abf8f41097dae398a
ziphozenkos <ziphomthombe478@gmail.com>
2023-03-30 06:44:42 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - function that return before main
+ * Return: nothing
+ */
+
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
