d475051af065cb5cec72c8f9e06070e1729e73d3
ziphozenkos <ziphomthombe478@gmail.com>
2023-04-07 06:40:11 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * print_binary - prints the binary representation of number
+ * @n: binary number
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,12 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
