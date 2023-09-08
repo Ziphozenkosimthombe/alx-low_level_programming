193315624e8f873ef1b40e9c75e45915759fc908
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-08 04:49:29 -0700

    hash_tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "./hash_tables.h"
+
+/**
+ * hash_table_print - the print of the hash table.
+ * @ht: hash table pointer to be printed.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int a = 0;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (; a < ht->size; a++)
+	{
+		if (ht->array[a] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[a];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
+		
